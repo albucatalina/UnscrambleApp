bc3c6fc417f512fff198893c78739472917b5de1
Albu Catalina <87013834+albucatalina@users.noreply.github.com>
2024-08-04 14:53:01 +0300

    Improve code coverage
    Test the case when the user skips a word

--- app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -69,6 +69,22 @@ class GameViewModelTest {
         assertTrue(currentGameUiState.isGameOver)
     }
 
+    @Test
+    fun gameViewModel_WordSkipped_ScoreUnchangedAndWordCountIncreased(){
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambleWord)
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        val lastWordCount= currentGameUiState.currentWordCount
+        viewModel.skipWord()
+
+        currentGameUiState = viewModel.uiState.value
+        assertEquals(lastWordCount + 1, currentGameUiState.currentWordCount)
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+    }
+
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
     }
